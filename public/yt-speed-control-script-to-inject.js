30cdd0d318053542f1300e635d673507abd1bb5b
avi12 <avi6106@gmail.com>
2021-01-29 23:07:34 +0200

    Reinitialized the project with Vue CLI 3

---
+++ public/yt-speed-control-script-to-inject.js
@@ -0,0 +1,26 @@
+let gPlayer;
+
+// A function that YouTube expects, which
+// if is present - will called by YouTube when
+// the player is ready, with the player
+// instance being in the parameter
+function onYouTubePlayerReady(player) {
+  gPlayer = player;
+
+  // https://developers.google.com/youtube/iframe_api_reference#Playback_rate
+  player.setPlaybackRate(getSpeed());
+}
+
+// The "data-yt-speed-control" attribute on the HTML
+// is the speed that the user is preferring
+const observerHtml = new MutationObserver(() => {
+  gPlayer?.setPlaybackRate(getSpeed());
+});
+observerHtml.observe(document.documentElement, {
+  attributes: true,
+  attributeFilter: ["data-yt-speed-control"]
+});
+
+function getSpeed() {
+  return Number(document.documentElement.dataset.ytSpeedControl);
+}
---
+++ src/App.vue
@@ -0,0 +1,143 @@
+<template>
+  <v-app>
+    <h2 class="red--text my-4 font-weight-medium">YouTube Speed Control</h2>
+    <v-slider
+      :max="labelSpeedsReversed.length - 1"
+      :thumb-color="isSpeedCustom ? 'transparent' : ''"
+      :tick-labels="labelSpeedsReversed"
+      :value="iSpeedReversed"
+      class="flex-grow-0 margin-right--match"
+      tick-size="5"
+      vertical
+      @change="setSpeed"
+    ></v-slider>
+
+    <v-radio-group
+      :value="isSpeedCustom"
+      class="mx-auto mb-4 mt-0"
+      hide-details
+      row
+      @change="isSpeedCustom = true"
+    >
+      <v-radio :value="true">
+        <template v-slot:label>
+          <v-text-field
+            :value="speedCustom"
+            label="Custom"
+            type="number"
+            @input="setCustomSpeed"
+          ></v-text-field>
+        </template>
+      </v-radio>
+    </v-radio-group>
+  </v-app>
+</template>
+
+<script>
+import { speeds } from "@/extension/content/yt-speed-control-setup-utils";
+
+export default {
+  name: "App",
+  props: {
+    pSpeed: Number
+  },
+  data(vm) {
+    return {
+      iSpeed: vm.pISpeed,
+      iSpeedReversed: "", // Default: Normal
+      speedsReversed: [...speeds].reverse(),
+      // Reversing because Vuetify is flipping the array
+      // Spreading because Vuetify mutates the array for some reason
+      speedCustom: vm.pSpeed,
+      isSpeedCustom: speeds.indexOf(vm.speedCustom) === -1
+    };
+  },
+  watch: {
+    speedCustom(speed) {
+      chrome.storage.local.set({ speed });
+    },
+    iSpeedReversed(iSpeedReversed) {
+      const iSpeed = this.speedsReversed.length - iSpeedReversed - 1;
+      const speed = speeds[iSpeed];
+      chrome.storage.local.set({ speed });
+    }
+  },
+  computed: {
+    labelSpeedsReversed() {
+      return this.speedsReversed.map(speed => {
+        if (speed === 1) {
+          return "Normal";
+        }
+        return speed;
+      });
+    }
+  },
+  methods: {
+    setSpeed(iSpeedReversed) {
+      this.isSpeedCustom = false;
+      this.iSpeedReversed = iSpeedReversed;
+      const iSpeed = this.speedsReversed.length - iSpeedReversed - 1;
+      this.speedCustom = speeds[iSpeed];
+    },
+    setCustomSpeed(speedCustom) {
+      this.speedCustom = Number(speedCustom);
+      this.isSpeedCustom = true;
+      const iSpeed = speeds.indexOf(this.speedCustom);
+      const isSpeedInArray = iSpeed > -1;
+      if (isSpeedInArray) {
+        this.iSpeedReversed = this.speedsReversed.length - iSpeed - 1;
+      }
+    }
+  },
+  created() {
+    this.speedCustom = this.pSpeed;
+    const iSpeed = speeds.indexOf(this.speedCustom);
+    if (iSpeed > -1) {
+      this.iSpeedReversed = this.speedsReversed.length - iSpeed - 1;
+      this.isSpeedCustom = false;
+    } else {
+      this.isSpeedCustom = true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.margin-right--match {
+  margin-right: 110px;
+}
+</style>
+
+<style>
+html {
+  overflow-y: hidden;
+}
+
+/*noinspection CssUnusedSymbol*/
+#app {
+  font-family: system-ui, sans-serif;
+  text-align: center;
+  width: 18em;
+  font-size: 17px;
+}
+
+/*noinspection CssUnusedSymbol*/
+.thumb--invisible .v-slider__thumb:before {
+  opacity: 0 !important;
+}
+
+/*noinspection CssUnusedSymbol*/
+.v-slider--vertical {
+  margin-bottom: 0;
+}
+
+/*noinspection CssUnusedSymbol*/
+.v-application--wrap {
+  min-height: 100%;
+}
+
+/*noinspection CssUnusedSymbol*/
+.v-label {
+  width: 87px;
+}
+</style>
