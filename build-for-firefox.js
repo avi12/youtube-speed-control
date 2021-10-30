afca4fb27e7b012a2fe77a66af98d7474694ae40
avi12 <avi6106@gmail.com>
2021-10-30 16:54:02 +0300

    Fixed Firefox's build script

--- build-for-firefox.js
+++ build-for-firefox.js
@@ -10,8 +10,8 @@ if (!fs.existsSync(dirZip)) {
   process.exit();
 }
 
-function getManifest(zip) {
-  return JSON.parse(zip.getEntry("manifest.json").getData());
+function getManifest(zip, entry) {
+  return JSON.parse(zip.getEntry(entry).getData());
 }
 
 function getModifiedManifest(manifest) {
@@ -21,18 +21,30 @@ function getModifiedManifest(manifest) {
   return manifest;
 }
 
+function rebuildZipForFirefox(zipName, version) {
+  const zip = new AdmZip(zipName);
+  const manifest = getModifiedManifest(getManifest(zip, "manifest.json"));
+
+  zip.addFile("manifest.json", Buffer.from(JSON.stringify(manifest), "utf-8"));
+  zip.writeZip(argv.i.replace("{version}", version + "__adapted_for_firefox"));
+}
+
+function rebuildZipSourceForFirefox(zipName, version) {
+  const zip = new AdmZip(zipName);
+  const manifest = getModifiedManifest(getManifest(zip, "dist/manifest.json"));
+
+  zip.addFile("manifest.json", Buffer.from(JSON.stringify(manifest, null, 2), "utf-8"));
+  zip.writeZip(argv.i.replace("{version}", version + "__adapted_for_firefox-source"));
+}
+
 function init() {
   const { version } = JSON.parse(
     fs.readFileSync("package.json", { encoding: "utf-8" }).toString()
   );
   const name = argv.i;
-  const zipName = name.replace("{version}", version);
 
-  const zip = new AdmZip(zipName);
-  const manifest = getModifiedManifest(getManifest(zip));
-
-  zip.addFile("manifest.json", Buffer.from(JSON.stringify(manifest), "utf-8"));
-  zip.writeZip(argv.i.replace("{version}", version + "__adapted_for_firefox"));
+  rebuildZipForFirefox(name.replace("{version}", version), version);
+  rebuildZipSourceForFirefox(name.replace("{version}", version + "-source"), version);
 }
 
 init();
